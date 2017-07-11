089510995f989b3681ac41274db5db9d5c84aab6
Yihui Xiong <yihui.xiong@hotmail.com>
2017-07-11 11:26:22 +0800

    add dueros oauth support

--- avs/auth.py
+++ avs/auth.py
@@ -1,18 +1,29 @@
-import os
+
+import sys
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
 import json
 import uuid
 import requests
+import datetime
 
-
-configuration_file = os.path.join(os.path.expanduser('~'), '.alexa.json')
+from avs.config import DEFAULT_CONFIG_FILE
 
 
 class MainHandler(tornado.web.RequestHandler):
-    def initialize(self, config):
+    def initialize(self, config, output):
         self.config = config
+        self.output = output
+
+        if ('host_url' in self.config) and self.config['host_url'] == 'dueros-h2.baidu.com':
+            self.token_url = 'https://openapi.baidu.com/oauth/2.0/token'
+            self.oauth_url = 'https://openapi.baidu.com/oauth/2.0/authorize'
+            self.scope = 'basic'
+        else:
+            self.token_url = 'https://api.amazon.com/auth/o2/token'
+            self.oauth_url = 'https://www.amazon.com/ap/oa'
+            self.scope = 'alexa:all'
 
     @tornado.web.asynchronous
     def get(self):
@@ -26,13 +37,20 @@ class MainHandler(tornado.web.RequestHandler):
                 "grant_type": "authorization_code",
                 "redirect_uri": redirect_uri
             }
-            url = "https://api.amazon.com/auth/o2/token"
-            r = requests.post(url, data=payload)
+
+            r = requests.post(self.token_url, data=payload)
             config = r.json()
             self.config['refresh_token'] = config['refresh_token']
-            print('save the configuration to {}'.format(configuration_file))
+
+            if 'access_token' in config:
+                date_format = "%a %b %d %H:%M:%S %Y"
+                expiry_time = datetime.datetime.utcnow() + datetime.timedelta(seconds=config['expires_in'])
+                self.config['expiry'] = expiry_time.strftime(date_format)
+                self.config['access_token'] = config['access_token']
+
+            print('save the configuration to {}'.format(self.output))
             print(json.dumps(self.config, indent=4))
-            with open(configuration_file, 'w') as f:
+            with open(self.output, 'w') as f:
                 json.dump(self.config, f, indent=4)
 
             self.write('Succeed to login Amazon Alexa Voice Service')
@@ -48,15 +66,15 @@ class MainHandler(tornado.web.RequestHandler):
                         }
                     }
                 })
-                url = "https://www.amazon.com/ap/oa"
                 payload = {
                     "client_id": self.config['client_id'],
-                    "scope": "alexa:all",
+                    "scope": self.scope,
                     "scope_data": scope_data,
                     "response_type": "code",
                     "redirect_uri": redirect_uri
                 }
-                req = requests.Request('GET', url, params=payload)
+
+                req = requests.Request('GET', self.oauth_url, params=payload)
                 p = req.prepare()
                 self.redirect(p.url)
             else:
@@ -65,33 +83,34 @@ class MainHandler(tornado.web.RequestHandler):
                 tornado.ioloop.IOLoop.instance().stop()
 
 
-def login(config):
-    print("Go to http://127.0.0.1:3000 to sign up or login Amazon Alexa Voice Service")
-    application = tornado.web.Application([(r".*", MainHandler, dict(config=config))])
+def login(config, output):
+    application = tornado.web.Application([(r".*", MainHandler, dict(config=config, output=output))])
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(3000)
     tornado.ioloop.IOLoop.instance().start()
     tornado.ioloop.IOLoop.instance().close()
 
 
-def setup(config):
+def setup(config, output):
     import threading
     import webbrowser
 
-    t = threading.Thread(target=login, args=(config,))
+    t = threading.Thread(target=login, args=(config, output))
     t.start()
+    print("Go to http://127.0.0.1:3000 to start")
     webbrowser.open('http://127.0.0.1:3000')
     t.join()
 
 
 def main():
-    import sys
-
     if len(sys.argv) < 2:
-        print('Usage: {} config.json'.format(sys.argv[0]))
-        print('\nWhen you succeed to login Amazon Alexa Voice Service, it will save the configuration to {}'.format(
-            configuration_file))
+        print('Usage: {} config.json [output.json]'.format(sys.argv[0]))
         sys.exit(1)
+
+    if len(sys.argv) < 3:
+        output = DEFAULT_CONFIG_FILE
+    else:
+        output = sys.argv[2]
 
     with open(sys.argv[1], 'r') as f:
         config = json.load(f)
@@ -104,7 +123,7 @@ def main():
         if ('refresh_token' in config) and config['refresh_token']:
             print('Already authorized')
         else:
-            setup(config)
+            setup(config, output)
 
 
 if __name__ == '__main__':
---
+++ avs/config.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+
+DEFAULT_CONFIG_FILE = os.path.join(os.path.expanduser('~'), '.avs.json')
