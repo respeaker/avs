f78d5bdab54478334b0bff37fbf55b1d4785cbff
Yihui Xiong <yihui.xiong@hotmail.com>
2018-03-27 20:47:35 +0800

    bugfix

--- avs/mpg123_player.py
+++ avs/mpg123_player.py
@@ -25,24 +25,57 @@ class Player(object):
         self.callbacks = {}
         self.process = None
         self.state = 'NULL'
+        self.audio = None
+
+        self.event = threading.Event()
+        t = threading.Thread(target=self._run)
+        t.daemon = True
+        t.start()
+
+    def _run(self):
+        while True:
+            self.event.wait()
+            self.event.clear()
+            print('Playing {}'.format(self.audio))
+            self.process = subprocess.Popen(['mpg123', self.audio])
+            self.process.wait()
+            print('Finished {}'.format(self.audio))
+
+            if not self.event.is_set():
+                self.on_eos()
 
     def play(self, uri):
-        self.state = 'PLAYING'
+        print('play()')
+
+
         if uri.startswith('file://'):
             uri = uri[7:]
+
+        self.audio = uri
+        self.event.set()
+
+        if self.process and self.process.poll() == None:
+            if self.state == 'PAUSED':
+                os.kill(self.process.pid, signal.SIGCONT)
+            self.process.terminate()
+            
+        self.state = 'PLAYING'
         
-        print(uri)
-        self.process = popen(['mpg123', uri], self.on_eos)
+        print('set play event')
 
     def stop(self):
-        if self.state == 'PLAYING':
-            self.state = 'NULL'
+        if self.process and self.process.poll() == None:
+            if self.state == 'PAUSED':
+                os.kill(self.process.pid, signal.SIGCONT)
             self.process.terminate()
+        self.state = 'NULL'
 
     def pause(self):
         if self.state == 'PLAYING':
             self.state = 'PAUSED'
             os.kill(self.process.pid, signal.SIGSTOP)
+
+        print('pause()')
 
     def resume(self):
         if self.state == 'PAUSED':
@@ -57,7 +90,7 @@ class Player(object):
         self.callbacks[name] = callback
 
     def on_eos(self):
-        self._state = 'NULL'
+        self.state = 'NULL'
         if 'eos' in self.callbacks:
             self.callbacks['eos']()
 
